ab7f63f661297caa1050b5ed3e35a9f1069b1508
SameerBramhecha <bramhechas89@gmail.com>
2023-12-22 15:48:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         <Route path='/' element={<LoginPage />} />
           <Route path='/dashboard' element={<Dashboard />} />
       </Routes>
+      <LoginPage />
     </div>
   );
 }
